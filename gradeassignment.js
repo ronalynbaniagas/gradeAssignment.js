ff5c97dd3ef4e9c7a64437b6c03f25bd3d770e0e
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-10-11 09:29:10 +0800

    Add files via upload

---
+++ gradeassignment.js
@@ -0,0 +1,10 @@
+// ELSE IF STATEMENT
+
+let grade = 95;
+if (grade >= 90) {
+    console.log("EXCELLENT");
+} else if (grade >= 80) {
+    console.log("GOOD");
+}else {
+    console.log("NEEDS IMPROVEMENT");
+}
